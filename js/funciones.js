d7f1e21759380a7b6ea9b643587143dcc7015292
Miguel Dominguez Chamorro <mdchamorro7@gmail.com>
2020-03-02 18:09:10 +0100

    fetch-planetas

--- js/funciones.js
+++ js/funciones.js
@@ -13,5 +13,58 @@ function barClicked() {
   nav.classList.toggle('visible');
 }
 
+//Crea la estructura para cada personaje
+function creaPersonaje(personaje) {
+  console.log(personaje.name);
+  let articlePersonajes = document.querySelector(".article-personajes");
+  let divPersonaje = document.createElement("div");
+  divPersonaje.innerHTML = `
+  <div data="card header">
+  <div class="card-image">
+    <img src="${personaje.image}" alt="${personaje.name}">
+  </div>
+    <div class="card-nombre">
+      <h2 class="nombre">${personaje.name}</h2>
+    </div>
+  </div>
+    <div class="card-status">
+      <h4>STATUS</h4>
+      <p>${personaje.name}</p>
+    </div>
+    <div class="card-species">
+      <h4>SPECIE</h4>
+      <p>${personaje.species}</p>
+    </div>
+    <div class="card-gender">
+      <h4>GENDER</h4>
+      <p>${personaje.gender}</p>
+    </div>
+    <div class="card-location">
+      <h4>LOCATION</h4>
+      <p>${personaje.location.name}</p>
+    </div>
+  </div>`;
+  articlePersonajes.appendChild(divPersonaje);
+  }
+
 // llamadas a la api
+
+function planeta_tierra () {
+  fetch('https://rickandmortyapi.com/api/location/1/')
+    .then(response => response.json())
+    .then(data => {
+      personajes(data);})
+}
+    
+function personajes(data) {    
+    data.residents.forEach(element => {
+      fetch(element)
+      .then(response => response.json())
+      .then(personaje => 
+        creaPersonaje(personaje));
+    });
+}
+
+planeta_tierra()
+
 
