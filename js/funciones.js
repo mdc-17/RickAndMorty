a34702de9d1650c594325598a18d61d65229c1a5
Miguel Dominguez Chamorro <mdchamorro7@gmail.com>
2020-03-05 16:45:49 +0100

    ultimo ultimo

--- js/funciones.js
+++ js/funciones.js
@@ -80,7 +80,7 @@ clickPlaneta()
 
 function personajes(data) {  
     let articlePersonajes = document.querySelector(".planetas");
-    articlePersonajes.innerHTML = '<div><a href="../mapa.html"> <h1> VOLVER ATRAS </h1></a></div>';
+    articlePersonajes.innerHTML = `<div><a href="../mapa.html"> <h1> VOLVER ATRAS </h1></a></div>`;
   data.residents.forEach(element => {
       fetch(element)
       .then(response => response.json())
@@ -93,37 +93,37 @@ function personajes(data) {
 //CAPITULOS
 
 //Crea la estructura para cada personaje
-function creaPersonaje(personaje) {
-  let articlePersonajes = document.querySelector(".planetas");
-  let divPersonaje = document.createElement("div");
-  divPersonaje.innerHTML = `
-  <div data="card header">
-  <div class="card-image">
-    <img src="${personaje.image}" alt="${personaje.name}">
-  </div>
-    <div class="card-nombre">
-      <h2 class="nombre">${personaje.name}</h2>
-    </div>
-  </div>
-    <div class="card-status">
-      <h4>STATUS</h4>
-      <p>${personaje.name}</p>
-    </div>
-    <div class="card-species">
-      <h4>SPECIE</h4>
-      <p>${personaje.species}</p>
-    </div>
-    <div class="card-gender">
-      <h4>GENDER</h4>
-      <p>${personaje.gender}</p>
-    </div>
-    <div class="card-location">
-      <h4>LOCATION</h4>
-      <p>${personaje.location.name}</p>
-    </div>
-  </div>`;
-  articlePersonajes.appendChild(divPersonaje);
-  }
+// function creaPersonaje(personaje) {
+//   let articlePersonajes = document.querySelector(".planetas");
+//   let divPersonaje = document.createElement("div");
+//   divPersonaje.innerHTML = `
+//   <div data="card header">
+//   <div class="card-image">
+//     <img src="${personaje.image}" alt="${personaje.name}">
+//   </div>
+//     <div class="card-nombre">
+//       <h2 class="nombre">${personaje.name}</h2>
+//     </div>
+//   </div>
+//     <div class="card-status">
+//       <h4>STATUS</h4>
+//       <p>${personaje.name}</p>
+//     </div>
+//     <div class="card-species">
+//       <h4>SPECIE</h4>
+//       <p>${personaje.species}</p>
+//     </div>
+//     <div class="card-gender">
+//       <h4>GENDER</h4>
+//       <p>${personaje.gender}</p>
+//     </div>
+//     <div class="card-location">
+//       <h4>LOCATION</h4>
+//       <p>${personaje.location.name}</p>
+//     </div>
+//   </div>`;
+//   articlePersonajes.appendChild(divPersonaje);
+//   }
 
 // llamadas a la api
 function capitulo(id) {
