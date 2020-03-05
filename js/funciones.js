85e7b9e2b3743d130e23cd2daff36278b28b3aeb
Miguel Dominguez Chamorro <mdchamorro7@gmail.com>
2020-03-05 14:31:08 +0100

    añadidos los capitulos

--- js/funciones.js
+++ js/funciones.js
@@ -13,9 +13,10 @@ function barClicked() {
   nav.classList.toggle('visible');
 }
 
+
 //Crea la estructura para cada personaje
 function creaPersonaje(personaje) {
-  let articlePersonajes = document.querySelector(".article-personajes");
+  let articlePersonajes = document.querySelector(".planetas");
   let divPersonaje = document.createElement("div");
   divPersonaje.innerHTML = `
   <div data="card header">
@@ -47,45 +48,39 @@ function creaPersonaje(personaje) {
   }
 
 // llamadas a la api
+console.log("hola");
+function planeta(id) {
+    fetch(`https://rickandmortyapi.com/api/location/${id}/`)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        personajes(data);})
+  }
 
-function planeta_tierra() {
-  fetch('https://rickandmortyapi.com/api/location/20/')
-    .then(response => response.json())
-    .then(data => {
-      personajes(data);})
+
+/* planeta tirra = 20
+ciudadela = 3
+apocal = 8
+anatomy = 5
+cable = 6
+*/ 
+
+function clickPlaneta() {
+    let planetas = document.querySelectorAll('.planeta');
+    planetas.forEach(element => {
+        let id = element.getAttribute("id")
+        element.onclick =  () => { 
+            //event.prevenDefault()
+            planeta(id) 
+        }
+    })
 }
 
-function planeta_ciudadela() {
-  fetch('https://rickandmortyapi.com/api/location/3/')
-    .then(response => response.json())
-    .then(data => {
-      personajes(data);})
-}
+clickPlaneta()
 
-function planeta_apocal() {
-  fetch('https://rickandmortyapi.com/api/location/8/')
-    .then(response => response.json())
-    .then(data => {
-      personajes(data);})
-}
-
-function planeta_anatomy() {
-  fetch('https://rickandmortyapi.com/api/location/5/')
-    .then(response => response.json())
-    .then(data => {
-      personajes(data);})
-}
-
-
-function planeta_cable() {
-  fetch('https://rickandmortyapi.com/api/location/6/')
-    .then(response => response.json())
-    .then(data => {
-      personajes(data);})
-}
-    
 function personajes(data) {  
-  
+    let articlePersonajes = document.querySelector(".planetas");
+    articlePersonajes.innerHTML = '<div><a href="../mapa.html"> <h1> VOLVER ATRAS </h1></a></div>';
   data.residents.forEach(element => {
       fetch(element)
       .then(response => response.json())
@@ -95,3 +90,84 @@ function personajes(data) {
 }
 
 
+//CAPITULOS
+
+//Crea la estructura para cada personaje
+function creaPersonaje(personaje) {
+  let articlePersonajes = document.querySelector(".planetas");
+  let divPersonaje = document.createElement("div");
+  divPersonaje.innerHTML = `
+  <div data="card header">
+  <div class="card-image">
+    <img src="${personaje.image}" alt="${personaje.name}">
+  </div>
+    <div class="card-nombre">
+      <h2 class="nombre">${personaje.name}</h2>
+    </div>
+  </div>
+    <div class="card-status">
+      <h4>STATUS</h4>
+      <p>${personaje.name}</p>
+    </div>
+    <div class="card-species">
+      <h4>SPECIE</h4>
+      <p>${personaje.species}</p>
+    </div>
+    <div class="card-gender">
+      <h4>GENDER</h4>
+      <p>${personaje.gender}</p>
+    </div>
+    <div class="card-location">
+      <h4>LOCATION</h4>
+      <p>${personaje.location.name}</p>
+    </div>
+  </div>`;
+  articlePersonajes.appendChild(divPersonaje);
+  }
+
+// llamadas a la api
+function capitulo(id) {
+    fetch(`https://rickandmortyapi.com/api/episode/${id}/`)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        captipersonajes(data);})
+  }
+
+
+
+function clickCapitulo() {
+    let chapter = document.querySelectorAll('.capitulo');
+    chapter.forEach(element => {
+        let id = element.getAttribute("id")
+        element.onclick =  () => { 
+            //event.prevenDefault()
+            capitulo(id) 
+        }
+    })
+}
+
+clickCapitulo()
+
+function personajes(data) {  
+    let articlePersonajes = document.querySelector(".capitulos");
+    articlePersonajes.innerHTML = '<div><a href="../mapa.html"> <h1> VOLVER ATRAS </h1></a></div>';
+  data.residents.forEach(element => {
+      fetch(element)
+      .then(response => response.json())
+      .then(personaje => 
+        creaPersonaje(personaje));
+    });
+}
+
+
+function captipersonajes(data) {  
+  let articlePersonajes = document.querySelector(".planetas");
+  articlePersonajes.innerHTML = '<div><a href="../capitulos.html"> <h1> VOLVER ATRAS </h1></a></div>';
+data.characters.forEach(element => {
+    fetch(element)
+    .then(response => response.json())
+    .then(personaje => 
+      creaPersonaje(personaje));
+  });
+}
